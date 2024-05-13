d11cd16539d395b71ea84ace502f908f1d48e444
xinnj <xinnj@hotmail.com>
2024-05-13 18:06:49 +0800

    Add REMDME

--- config/ext/oidc.php
+++ config/ext/oidc.php
@@ -3,6 +3,7 @@ $filter->oidc = new stdclass();
 $filter->oidc->login = new stdclass();
 $filter->oidc->login->paramValue['scope'] = 'reg::any';
 
+$config->oidc = new stdclass();
 $config->oidc->provider_url = "";
 $config->oidc->clientId = "";
 $config->oidc->clientSecret = "";
